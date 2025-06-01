2887bea897d4386219ca75cbacc2ed27b9408a16
Weronika <weronika_dabrowska@yahoo.co.uk>
2025-06-01 14:29:38 +0100

    Add forecast data in JavaScript + Adjusted the size of the icon in CSS

--- src/index.js
+++ src/index.js
@@ -54,6 +54,13 @@ function handelSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "04b5fc5oc9b0fe6f93atfbe5cc26da36";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -61,28 +68,33 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  let forecastElement = document.querySelector("#forecast");
+  console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">⛅</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temps">
          <div class="weather-forecast-temp">
-           <strong>15°</strong>
+           <strong>${Math.round(day.temperature.maximum)}°</strong>
          </div>
-      <div class="weather-forecast-temp">9°</div>
-    </div>
-  </div>
+         <div class="weather-forecast-temp">${Math.round(
+           day.temperature.minimum
+         )}°</div>
+        </div>
+      </div>
 `;
+    }
   });
 
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
@@ -90,4 +102,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handelSearchSubmit);
 
 searchCity("London");
-displayForecast();
